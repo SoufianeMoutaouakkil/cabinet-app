e07b17b82516fc60daab1d00234e2383eda9a52c
SoufianeMoutaouakkil <115849188+SoufianeMoutaouakkil@users.noreply.github.com>
2024-03-25 22:32:19 +0000

    Add consultationsSlice to store

--- client/src/services/store/index.js
+++ client/src/services/store/index.js
@@ -4,11 +4,13 @@ import { combineReducers } from "redux";
 import usersReducer from "./slices/usersSlice";
 import authReducer from "./slices/authSlice";
 import patientsReducer from "./slices/patientsSlice";
+import consultationsReducer from "./slices/consultationsSlice";
 
 export default configureStore({
   reducer: combineReducers({
     auth: authReducer,
     users: usersReducer,
     patients: patientsReducer,
+    consultations: consultationsReducer,
   }),
 });
---
+++ client/src/services/store/slices/consultationsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addReducerApiCases } from "../../shared/reducerManager";
+import { generateApiActions, generateExportedActions } from "../../shared/actionsManager";
+
+const initialState = {};
+
+const consultationsApiActions = generateApiActions("consultations");
+
+const consultationsSlice = createSlice({
+  name: "consultations",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    addReducerApiCases(builder, consultationsApiActions);
+  },
+});
+
+export const {
+  consultationsGetAll,
+  consultationsGetById,
+  consultationsCreate,
+  consultationsUpdate,
+  consultationsRemove,
+  consultationsSearch,
+} = generateExportedActions("consultations", consultationsApiActions);
+
+export default consultationsSlice.reducer;
