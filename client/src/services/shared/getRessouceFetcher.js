fd7355af1eebd62314dc7677556219af3ebf6d89
SoufianeMoutaouakkil <115849188+SoufianeMoutaouakkil@users.noreply.github.com>
2024-03-25 21:40:07 +0000

    Refactor getRessouceFetcher functions to use destructuring. createAsyncThunk take one arg.

--- client/src/services/shared/getRessouceFetcher.js
+++ client/src/services/shared/getRessouceFetcher.js
@@ -3,30 +3,38 @@ import { getApiCaller } from "./getBackCaller";
 export const getRessouceFetcher = (resource) => {
     const apiCaller = getApiCaller(resource);
 
-    const getById = async (id, opts = {}) => {
-        return await apiCaller({ id, method :"GET", ...opts });
+    const getById = async ({ id, opts = {} }) => {
+        return await apiCaller({ id, method: "GET", ...opts });
     };
-    const getAll = async (opts = {}) => {
+    const getAll = async ({ opts = {} }) => {
         return await apiCaller({ method: "GET", ...opts });
     };
-    const create = async (data, opts = {}) => {
+    const create = async ({ data, opts = {} }) => {
+        data = { data };
         return await apiCaller({ method: "POST", data, ...opts });
     };
-    const update = async (id, data, opts = {}) => {
-        return await apiCaller({ id, method: "PUT", data, ...opts });
+    const update = async ({ id, data, opts = {} }) => {
+        data = { data };
+        return await apiCaller({
+            id,
+            method: "PUT",
+            data,
+            ...opts,
+        });
     };
-    const remove = async (id, opts = {}) => {
+    const remove = async ({ id, opts = {} }) => {
         return await apiCaller({ id, method: "DELETE", ...opts });
     };
-    const search = async (query, opts = {}) => {
+    const search = async ({ query, opts = {} }) => {
+        query = !query ? { field: "_id", operator: "exists" } : query;
+        console.log("search: query: ", query);
         // parse query as stringquery
         if (typeof query === "object") {
             query = JSON.stringify(query);
             query = "query=" + encodeURIComponent(query);
         }
         opts.query = query;
-       
-        
+
         return await apiCaller({ method: "GET", ...opts });
     };
     const call = async (opts) => {
