d681e1d112b355694c9f7fb26f02fdaf9af36baa
SoufianeMoutaouakkil <115849188+SoufianeMoutaouakkil@users.noreply.github.com>
2024-03-26 22:35:36 +0000

    Refactor login UI and root page

---
+++ client/src/components/common/Copyright.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+function Copyright(props) {
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
+            SMDEV
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
+
+export default Copyright;
--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -2,30 +2,44 @@ import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router";
 import { useSelector } from "react-redux";
 import Header from "../components/common/Header";
+import { ThemeProvider } from "@mui/material";
+import Container from "@mui/material/Container";
+import Copyright from "../components/common/Copyright";
+import defaultTheme from "../themes/defaultTheme";
+import Login from "./auth/Login";
 
 function Root() {
     const navigate = useNavigate();
     const { authData } = useSelector((state) => state.auth);
     const currentPath = window.location.pathname;
     const [render, setRender] = useState(<></>);
+    const [isLogged, setIsLogged] = useState(false);
 
     useEffect(() => {
+        console.log("authData", authData);
         if (
             (!authData?.user || !authData?.token) &&
             !currentPath.includes("/login") &&
             !currentPath.includes("/register")
         ) {
-            navigate(`/auth/login`);
+            setRender(<Login />);
+            setIsLogged(false);
         } else {
+            setIsLogged(true);
             setRender(<Outlet />);
         }
     }, [authData, navigate, currentPath]);
 
     return (
-        <div>
-            <Header />
-            {render}
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <div>
+                    {isLogged && <Header />}
+                    {render}
+                </div>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
     );
 }
 
